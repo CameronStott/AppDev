86cea2d47adf8959be312608bd27b364193a2b23
CameronStott <40416068@live.napier.ac.uk>
2020-02-12 19:36:40 +0000

    second app using two activities

---
+++ app/src/main/java/com/example/quicklauncher/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.quicklauncher;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //attempts to launch a new activity within our app
+        Button secondActivityBtn = (Button) findViewById(R.id.secondActivityBtn);
+        secondActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntnet = new Intent(getApplicationContext(), SecondActivity.class);
+                startIntnet.putExtra("com.example.quickLauncher.SOMETHING","HELLO WORLD");
+                startActivity(startIntnet);
+            }
+        });
+
+        //attempt to launch an activity outside our app
+        Button googleBtn = (Button) findViewById(R.id.googleBtn);
+        googleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String google = "http://www.yahoo.com";
+                Uri webaddress = Uri.parse(google);
+
+                Intent goToGoogle = new Intent(Intent.ACTION_VIEW, webaddress);
+                if (goToGoogle.resolveActivity(getPackageManager()) != null){
+                    startActivity(goToGoogle);
+                }
+            }
+        });
+    }
+}
